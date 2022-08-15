dbf0401a2c60de9cedc04490836861fc22d06932
massu-159 <magjtwjp@gmail.com>
2022-08-15 23:59:59 +0900

    connect backend server

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ApolloClient, ApolloProvider } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(

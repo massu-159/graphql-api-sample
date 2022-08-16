bad74b80a12ba2c347161decd49ef9c335606138
massu-159 <magjtwjp@gmail.com>
2022-08-16 23:59:59 +0900

    get data at graphql

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,36 @@
-import { useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
 import './App.css'
 
-function App() {
+const BOOKS = gql`
+  query {
+    test {
+      title
+      author
+    }
+  }
+`;
 
+function Books() {
+  const { loading, error, data } = useQuery(BOOKS);
+  
+  if (loading) return <p>ロード中</p>;
+  if (error) return <p>エラー</p>;
+  
+  // dataに必要なデータが入っている
+  return data.test.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {author} : {title}
+      </p>
+    </div>
+  ))
+}
+
+function App() {
   return (
     <div className="App">
-      
+      <h2>GraphQL Client</h2>
+      <Books></Books>
     </div>
   )
 }
